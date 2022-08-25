97cf401108d383b84c395b13f79bd7a0db1143da
ShindanoSingi <shindanosingi@sekur.com>
2022-08-24 21:41:11 -0500

    m2

---
+++ .history/src/Components/AboutPage_20220824212839.js
@@ -0,0 +1,109 @@
+import React from "react"
+import "../Components/AboutPage.css"
+
+const AboutPage = () => {
+  return (
+    <div className="px-2 bg-light">
+      <h1>Shindano Singi</h1>
+      <p>
+        (207) 713-3345 ∙ 78 Maple Street, Apt. 31, Lewiston, ME 04240
+        ∙Shindanosingi1@gmail.com • Computer Science | Electrical Engineer
+        graduated in September 2021. Prepared to sit for FE. • Experience in
+        electromechanical industry work (textiles) machine use and repair •
+        Electrical Engineering skills: Systems Dynamics, Microprocessor Systems,
+        Digital Logic, Robotics, Analog and Digital Signal and Systems
+        Processing, Electronics Devices and Design, Micro Electro Mechanical
+        Systems (MEMS), Analog Circuit Design, Electromagnetic Fields,
+        Engineering Communications, PLC • Math Skills: Linear Algebra, and
+        Calculus Sequence, and Differential Equations, Statistics • Languages /
+        Software: Python, JAVA, Visual Basic Studio, SolidWorks, L-Edit, Matlab
+        • Strong oral and written (technical writing) skills English. Bilingual:
+        French & Swahili. EDUCATION • University of Southern Maine, Portland, ME
+        B.S. (Electrical Engineering), Graduated in May 2020 • Central Maine
+        Community College Associates Degree (Electromechanical Technology),
+        Greduated in June 2015 • Institut de la Fraternité, Tanzania Diploma in
+        Business and Administration, 2007 ● Scholarships include: WEX; College
+        Dean, Eagle Jackson, & Phi Theta Kappa Honor Society, Lewiston Auburn
+        Metropolitan Chamber Scholarship Recipient, President’s Honors List ●
+        Awards: Best 2nd Poster presented at ASEE 2019 conference at the
+        University at Buffalo, Undergraduate Research Opportunity (UROP), Maine
+        Space Grant Consortium (MSGC) PROFESSIONAL EXPERIENCE SIEMENS INC,
+        Scarborough, ME. March 2020 - Present HVAC SYSTEMS DESIGN and CONTROL
+        ENGINEER ● Participate in turnover meetings ● Do project labor plan in
+        excel ● Do valve schedule ● Do damper schedule ● Design HVAC systems
+        (AHU, Hot Water Systems, Chilled Water Systems...) in Autocad, based on
+        contract drawings, Div23, Div25 and Div26 requirements. ● Draw wiring
+        diagram of devices to controllers in Autocad. ● Create controllers and
+        points in Design Tools (DT) software. U.S. Citizen Shindano Singi (207)
+        713-3345 ∙ 78 Maple Street, Apt. 31, Lewiston, ME 04240
+        ∙Shindanosingi1@gmail.com ● Assign point addresses to devices such
+        temperature sensors, valve actuators, damper actuators, VFDs, Boilers,
+        fans... ● Create Commissioning Tools (CT) database ● Export points
+        database from DT to CT. ● Create a submittal for the PM. ● Do
+        Engineering Re-Estimate. Softwares ● Use "Wrike" software for project
+        management and communication ● Use "Teams" for communication (chat,
+        audio and video calls) ● Use "BluBeam" for documents management ● Use
+        "Si-Quote" for sales estimates ● Use "SAP" for time reporting ● Use
+        "DayForce" for time reporting ● Use "ABT" software for DXR controllers
+        management ● Microsoft Outlook ● Microsoft excel ● Microsoft Word ●
+        Microsoft PowerPoint ● Certification ● I have have obtained a lot of
+        certificates at Siemens: 1. HVAC 2. Valves selection 3. Damper selection
+        4. AHU 5. Siemens Controllers (PXCs and PXMs) 6. Siemens Controllers
+        (DXRs) 7. OSHA ... Awards ● I was awarded hard working and quick learner
+        employee at Siemens ABB, Auburn, ME September 2019 – March 2020 LEAN
+        Engineering Intern (September 2019 – March 2020) ● Use LEAN tools such
+        as GEMBA (Go and see how the job is done) on the production floor to
+        identify what needs to be improved and find the solution ● Provide
+        support to machine operators in both Vertical Machining Center (VMC) and
+        Weld Braze departments ● Make shadow boards for both VMC and Wels Braze
+        U.S. Citizen Shindano Singi (207) 713-3345 ∙ 78 Maple Street, Apt. 31,
+        Lewiston, ME 04240 ∙Shindanosingi1@gmail.com ● Write Standard Operation
+        Procedure (SOP) and organize tools to improve operators’ safety and
+        production flow ● Design and assemble parts in Moonshine shop using
+        various mechanical, electrical, hydraulic... tools such as Milling
+        machine (Bridgeport), heat bender, shop saws, ... ● Use hammer drill to
+        drill holes in the concrete ● Use 3D printer (Ender 3) to make plastic
+        parts ● Did PLC wiring on an automated machine ● Wired a single-phase
+        LEESON motor ● Disconnected and wired 480V and 120V gang boxes,
+        respectively ● Lead solution team meeting for both VMC and Weld Braze
+        departments ● Made chip guards for all VMC machines ● Added more
+        features to an existing table to satisfy work flow needs ● Made a table
+        for box making ● Successfully installed electrical, mechanical,
+        hydraulic and air operated devices ● Facilitate communication between
+        management, employees and maintenance ● Worked on individual Projects: o
+        Anode light Nomenclature o Preventive Maintenance (PM) tracking o Weld
+        Braze layout iteration ● Worked on team projects and presented them to
+        all leaders in the plant: o Printer Integration o Passivation Process o
+        Deburr Relocation University of Southern Maine (USM), Portland, ME
+        August 2018 – May 2020 Undergraduate Research Opportunities Program
+        Project (May 2019 - May 2020) Design and Redesign of Micro
+        Electro-Mechanical Systems (MEMS) Piezoelectric Vibration Energy and
+        sensing devices using Finite Element Modeling softwares: Solidworks and
+        ANSYS. Electronics Laboratory Technician, Electrical Engineering
+        Department (January 2019 – Sept. 2019) Provided technical support to
+        students performing electronics lab experiments. Maintained equipment.
+        Undergraduate Research Opportunities Program Assistant (Aug 2018 – May
+        2019) Test the Micro Electro-Mechanical Systems (MEMS) device:
+        Piezoelectric Vibration Energy Harvester, that I designed in the MEMS
+        class. Catholic Charities of Maine, Interpreter, Portland, ME July 2019
+        – March 2020 Interpret (oral) and translate (written) English into/from
+        French, Swahili, and Kibembe House of Languages, Interpreter, Portland,
+        ME Aug. 2015 – Present Interpret (oral) and translate (written) English
+        into/from French, Swahili, and Kibembe L.L. Bean Manufacturing,
+        Brunswick, ME July 2013 – Oct. 2018 U.S. Citizen Shindano Singi (207)
+        713-3345 ∙ 78 Maple Street, Apt. 31, Lewiston, ME 04240
+        ∙Shindanosingi1@gmail.com Bean’s Boots Machine Operator: monogram
+        textiles and maintain equipment(Oct. 2014 – Oct. 2018) Volunteer Stretch
+        Break Leader: led 50 + employees on health breaks (Oct. 2014 – Oct.
+        2016) Embroidery Machine Operator: embroider textiles and maintain
+        equipment (July 2013 – Oct. 2014). Central Maine Community College,
+        Electrician Intern, Auburn, ME May 2015 – Jul. 2015 Assisted certified
+        electricians with installing new electrical and electronic devices.
+        Amani Yao Atelier de Couture, Tailor, Tanzania 2007 – 2020 Designed,
+        measured, cut, stitched, and hand-tailored shirts, pants and dresses.
+      </p>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ .history/src/Components/Nav_20220824212132.js
@@ -0,0 +1,43 @@
+import React from "react"
+import "./Nav.css"
+import HomeHistoryPlaylistIcons from "./HomeHistoryPlaylistIcons"
+import SearchForm from "./SearchForm"
+import UserLoginIcon from "./UserLoginIcon"
+import { GoThreeBars } from "react-icons/go"
+import { Link } from "react-router-dom"
+
+// This is the search form. It conatins both the input box and the search icon
+const Nav = ({
+  searchedString,
+  handleSubmit,
+  handleChange,
+  forwardedRef,
+  userData,
+  getUserData,
+}) => {
+  // console.log(forwardedRef)
+
+  return (
+    <div className="flex justify-around w-screen py-2 bg-orange-600 md:py-4">
+      <HomeHistoryPlaylistIcons />
+      <Link to="/AboutPage">
+        <button
+          className="md:text-4xl go-button"
+          variant="contained"
+          href="#contained-buttons"
+        >
+          About
+        </button>
+      </Link>
+      <SearchForm
+        searchedString={searchedString}
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+      />
+      <UserLoginIcon forwardedRef={forwardedRef} userData={userData} />
+      <GoThreeBars className="text-lg md:text-3xl" />
+    </div>
+  )
+}
+
+export default Nav

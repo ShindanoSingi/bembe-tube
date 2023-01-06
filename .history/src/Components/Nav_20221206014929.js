d511080a4cf41030f1555b388f7d723bd271c32c
ShindanoSingi <shindanosingi@sekur.com>
2023-01-05 23:05:25 -0500

    m

---
+++ .history/src/Components/Nav_20221206014929.js
@@ -0,0 +1,36 @@
+import React from "react";
+import HomeHistoryPlaylistIcons from "./HomeHistoryPlaylistIcons";
+import SearchForm from "./SearchForm";
+import UserLoginIcon from "./UserLoginIcon";
+import { GoThreeBars } from "react-icons/go";
+import { Link } from "react-router-dom";
+
+// This is the search form. It conatins both the input box and the search icon
+const Nav = ({
+  searchedString,
+  handleSubmit,
+  handleChange,
+  forwardedRef,
+  userData,
+  getUserData
+}) => {
+  // console.log(forwardedRef)
+
+  return (
+    <nav>
+
+    </nav>
+    // <div className="nav-container">
+    //   <HomeHistoryPlaylistIcons />
+    //   <SearchForm
+    //     searchedString={searchedString}
+    //     handleChange={handleChange}
+    //     handleSubmit={handleSubmit}
+    //   />
+    //   <UserLoginIcon forwardedRef={forwardedRef} userData={userData} />
+    //   <GoThreeBars className="text-lg md:text-3xl hamburger" />
+    // </div>
+  );
+};
+
+export default Nav;
